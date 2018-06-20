83c843113d49a54783d4a622dfa443d290c6788f
gaoshiming <gaoshiming@zdns.cn>
2018-06-20 16:16:21 +0800

    add create ip-up and ip-down hook file

--- create_routes.rb
+++ create_routes.rb
@@ -1,29 +1,79 @@
 #!/usr/bin/env ruby
 #
-#
+
+VPN_GW = "192.168.42.1"
+
 
 def get_p(n)
     (Math.log(n.to_i) / Math.log(2)).to_i
 end
 
+cnRoutes = []
+fRoutes = []
+
 File.open "cn.txt", "r" do |f|
-File.open "cn.routes.txt", "w" do |f1|
+#File.open "cn.routes.txt", "w" do |f1|
     while l = f.gets do
         ip, count = l.split
         mask = 31 - get_p(count)
-        f1 << "#{ip}/#{mask}\n"
+#        f1 << "#{ip}/#{mask}\n"
+        cnRoutes << "#{ip}/#{mask}"
     end
-end
+#end
 end
 
 
 File.open "forin.txt", "r" do |f|
-File.open "forin.routes.txt", "w" do |f1|
+#File.open "forin.routes.txt", "w" do |f1|
     while l = f.gets do
         ip, count = l.split
         mask = 31 - get_p(count)
-        f1 << "#{ip}/#{mask}\n"
+#        f1 << "#{ip}/#{mask}\n"
+        fRoutes << "#{ip}/#{mask}"
     end
+#end
 end
+
+
+routes = fRoutes.map do |r|
+"route add #{r} ${VPN_GW}"
+end.join("\n")
+
+ip_up = <<EOF
+#!/bin/sh
+export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
+
+VPN_GW=#{VPN_GW}
+
+#{routes}
+
+EOF
+
+File.open("ip-up", "w") do |f|
+    f.write(ip_up)
 end
+
+
+
+routes = fRoutes.map do |r|
+"route delete #{r} ${VPN_GW}"
+end.join("\n")
+
+ip_down = <<EOF
+#!/bin/sh
+export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
+
+VPN_GW=#{VPN_GW}
+
+#{routes}
+
+EOF
+
+File.open("ip-down", "w") do |f|
+    f.write(ip_down)
+end
+
+
+
+
 
