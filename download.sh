71511a0c89ab3531009fe2e80304bdb119302cf8
gsmlg <gsmlg.com@gmail.com>
2018-06-20 22:14:01 +0800

    start v2

--- download.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-aria2c \
-    "https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest" \
-    -o "source.txt"
-
-
--- getv4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-file=source.txt
-
-grep ipv4 $file |awk -F'|' '{if($2 == "CN") { print $4" "$5 }}' > cn.txt
-
-grep ipv4 $file |awk -F'|' '{if($2 != "CN") { print $4" "$5 }}' |grep -v '*' > forin.txt
-
