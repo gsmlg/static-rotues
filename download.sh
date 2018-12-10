1971a72ddf6d1226bacdd086dc06feea9729c313
gsmlg <me@gsmlg.org>
2018-12-10 22:22:30 +0800

    support linux

--- download.sh
+++ download.sh
@@ -6,11 +6,22 @@ url=http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country-CSV.zip
 ${cmd} $url -o GeoLite2-Country-CSV.zip
 ${cmd} ${url}.md5 -o GeoLite2-Country-CSV.zip.md5
 
+MD5=$(which md5)
+MD5SUM=$(which md5sum)
 
-check=`md5 GeoLite2-Country-CSV.zip  |awk '{print $4}'`
+if [ -x "$MD5" ]
+do
+CHECKSUM=`md5 GeoLite2-Country-CSV.zip  |awk '{print $4}'`
+done
+
+if [ -x "$MD5SUM" ]
+do
+CHECKSUM=`md5sum GeoLite2-Country-CSV.zip  |awk '{print $1}'`
+done
+
 sum=`cat GeoLite2-Country-CSV.zip.md5`
 
-if [[ $check == $sum ]]
+if [[ $CHECKSUM == $sum ]]
 then
     echo "download success"
     rm GeoLite2-Country-CSV.zip.md5
