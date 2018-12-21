444646369b8a6df76ee5a0aef28efeec160ce766
gsmlg <me@gsmlg.org>
2018-12-22 01:36:57 +0800

    optmize openvpn

--- openvpn.rb
+++ openvpn.rb
@@ -28,33 +28,53 @@ data.slice(1, data.length).each do |r|
     end
 end
 
-rt = routes.map do |r|
-  network, mask = r.split("/")
-  lmask = IPAddr.new('255.255.255.255').mask(mask).to_s
-  %Q(route #{network} #{lmask} vpn_gateway)
-end
-
 FileUtils.rm_rf("mode1")
 FileUtils.mkdir("mode1")
 
-File.open "mode1/openvpn.txt", "w" do |f|
-    rt.each do |l|
+rts = []
+rtc = []
+
+routes.map do |r|
+  network, mask = r.split("/")
+  lmask = IPAddr.new('255.255.255.255').mask(mask).to_s
+  rtc << %Q(route #{network} #{lmask} vpn_gateway)
+  rts << %Q(push "route #{network} #{lmask} vpn_gateway")
+end
+
+File.open "mode1/openvpn-client.txt", "w" do |f|
+    rtc.each do |l|
         f << "#{l}\n"
     end
 end
 
-ct = cnRoutes.map do |r|
-  network, mask = r.split("/")
-  lmask = IPAddr.new('255.255.255.255').mask(mask).to_s
-  %Q(route #{network} #{lmask} net_gateway)
+File.open "mode1/openvpn-server.txt", "w" do |f|
+    rts.each do |l|
+        f << "#{l}\n"
+    end
 end
+
 
 FileUtils.rm_rf("mode2")
 FileUtils.mkdir("mode2")
 
-File.open "mode2/openvpn.txt", "w" do |f|
-    f << "redirect-gateway def1 bypass-dhcp ipv6\n"
-    rt.each do |l|
+cts = [%Q(push "redirect-gateway def1 bypass-dhcp ipv6")]
+ctc = ["redirect-gateway def1 bypass-dhcp ipv6"]
+
+cnRoutes.map do |r|
+  network, mask = r.split("/")
+  lmask = IPAddr.new('255.255.255.255').mask(mask).to_s
+  ctc << %Q(route #{network} #{lmask} net_gateway)
+  cts << %Q(push "route #{network} #{lmask} net_gateway")
+end
+
+File.open "mode2/openvpn-client.txt", "w" do |f|
+    ctc.each do |l|
+        f << "#{l}\n"
+    end
+end
+
+File.open "mode2/openvpn-server.txt", "w" do |f|
+    cts.each do |l|
         f << "#{l}\n"
     end
 end
